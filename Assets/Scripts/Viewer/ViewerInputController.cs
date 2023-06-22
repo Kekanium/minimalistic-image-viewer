95fbfc76903a96fbf44eb14af8f6db6b33a54b08
Stanislav <stas280501@yandex.ru>
2023-06-23 02:43:05 +0300

    Made simple Zoom

--- Assets/Scripts/Viewer/ViewerInputController.cs
+++ Assets/Scripts/Viewer/ViewerInputController.cs
@@ -1,13 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 
 public class ViewerInputController : MonoBehaviour {
     private Vector2 _touchStartPos;
     private const float MinSwipeDistance = 50f;
+    
+    private float _startingDistance;
+    private Vector3 _startingScale;
+
+   [SerializeField]private Image image;
 
     private void Update() {
         GoBack();
+        Zoom();
     }
 
     private void GoBack() {
@@ -19,7 +27,7 @@ public class ViewerInputController : MonoBehaviour {
 #endif
 
 #if UNITY_IOS
-        if (Input.touchCount > 0)
+        if (Input.touchCount = 1)
         {
             Touch touch = Input.GetTouch(0);
 
@@ -43,5 +51,33 @@ public class ViewerInputController : MonoBehaviour {
             }
         }
 #endif
+    }
+
+    private void Zoom() {
+        if (Input.touchCount == 2)
+        {
+            // Получаем данные о двух касаниях
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Проверяем, было ли только что начато движение (начальное положение пальцев)
+            if (touchOne.phase == TouchPhase.Began)
+            {
+                _startingDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                _startingScale = image.transform.localScale;
+            }
+            else if (touchZero.phase == TouchPhase.Moved && touchOne.phase == TouchPhase.Moved)
+            {
+                // Вычисляем текущее расстояние между пальцами
+                float currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
+
+                // Вычисляем разницу между начальным и текущим расстоянием
+                float scaleFactor = currentDistance / _startingDistance;
+
+                // Применяем масштабирование к объекту
+                image.transform.localScale = _startingScale * scaleFactor;
+                
+            }
+        }
     }
 }
